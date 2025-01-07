f4d3a5a4aacdf6c4757abb8633fc9ec102a58fb4
chen_1335 <3388508783@qq.com>
2025-01-07 18:48:43 +0800

    first edition

---
+++ src/main/java/com/chen1335/accessTransformerJs/AccessTransformerJs.java
@@ -0,0 +1,14 @@
+package com.chen1335.accessTransformerJs;
+
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.Mod;
+
+@Mod(AccessTransformerJs.MODID)
+public class AccessTransformerJs {
+    public static final String MODID = "access_transformer_js";
+
+    public AccessTransformerJs(IEventBus modEventBus, ModContainer modContainer) {
+
+    }
+}
